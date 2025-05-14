1aeda6493aa97463b05a43fe646c06e974958034
Anubis <41204624+gauravch45@users.noreply.github.com>
2025-05-15 02:13:59 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
 def call( String ProjectName, String ImageTag, String DockerHubUser, String Location){
-  sh "docker buildx build -t ${DockerHubUser}/${ProjectName}-${Location}:${ImageTag} ./${Location}"
+  sh "docker build -t ${DockerHubUser}/${ProjectName}-${Location}:${ImageTag} ./${Location}"
   echo "The build is Successfull"
 }
