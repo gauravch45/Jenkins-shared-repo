bf309effd347d8920dfd755fcede6d43172568c8
Anubis <41204624+gauravch45@users.noreply.github.com>
2025-05-22 17:21:55 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,4 +1,4 @@
 def call(String Directory){
   dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
-  dependencyCheckPublisher pattern: "**/dependency-check-report-${Directory}.xml"
+  dependencyCheckPublisher pattern: "**/dependency-check-report.xml"
 }
