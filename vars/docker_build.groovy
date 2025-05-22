d59597cfffd9f21b84c6352463baec88f6d2cf3f
Anubis <41204624+gauravch45@users.noreply.github.com>
2025-05-22 14:29:34 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
 def call( String ProjectName, String ImageTag, String DockerHubUser, String Location, String Args){
-  sh "docker build --build-arg ENV_FILE=.env --no-cache -t ${DockerHubUser}/${ProjectName}-${Location}:${ImageTag} ./${Location}"
+  sh "docker build --build-arg ENV_FILE=${Args} --no-cache -t ${DockerHubUser}/${ProjectName}-${Location}:${ImageTag} ./${Location}"
   echo "The build is Successfull"
 }
